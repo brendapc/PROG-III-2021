4f78c4d643304ceaa137a95f97ea931ea41c6658
Brenda Camara <brendacamara@outlook.com>
2021-07-31 23:40:25 -0300

    feat: add message when backend is down

--- js/tickets.js
+++ js/tickets.js
@@ -1,11 +1,14 @@
 (function (){
-    fetch('http://localhost:8989/tickets/destinations').then((response) => {
+    fetch('http://localhost:8989/tickets/destinations')
+    .then((response) => {
         return response.json()
-    }).then((result) => {
+    })
+    .then((result) => {
         result.forEach((destiny)=>{
             createItem(destiny)
         })
     })
+    .catch(() => {createWarning()})
 })()
 
 function createItem(destiny){
@@ -13,12 +16,23 @@ function createItem(destiny){
     const card = document.createElement('div')
     const title = document.createElement('h2')
     const img = document.createElement('img')
-
+    
     img.src = `./assets/${destiny.loc_chegada}.jpg`
     title.textContent = destiny.loc_chegada.replace('-', " ")
     card.appendChild(title)
     card.appendChild(img)
     card.classList.add("card")
     mainDiv.appendChild(card)
+}
 
+function createWarning(){
+    const mainDiv = document.querySelector('.main-content')
+    const card = document.createElement('div')
+    const title = document.createElement('h2')
+    
+    title.textContent = "É preciso iniciar o backend"
+    title.classList.add('fail-message')
+    card.appendChild(title)
+    //card.classList.add("card")
+    mainDiv.appendChild(card)
 }
